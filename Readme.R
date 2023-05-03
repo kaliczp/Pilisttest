b1d2c65faa157fdb4e0a06bc4d3f3e045aad5e4f
Péter Kalicz <kaliczp@gmail.com>
2023-05-03 09:46:01 +0200

    Set IO as factor and t.test example

--- Readme.R
+++ Readme.R
@@ -6,3 +6,7 @@ data.raw <- as.data.frame(read_excel("F_L_2023_complete_soil_dataset_T_probara.x
 names(data.raw) <- c("ID", "InOut", "wOkt", "wDec", "wFeb", "wMar", "meanh(cm)", "Dens(db/m2)")
 for(ttcolname in c("wOkt", "wDec", "wFeb", "wMar"))
     data.raw[, ttcolname] <- round(as.numeric(data.raw[, ttcolname]),3)
+data.raw[, "InOut"] <- factor(data.raw[, "InOut"])
+t.test(wOkt ~ InOut, data.raw)
+boxplot(wOkt ~ InOut, data.raw)
+t.test(wMar ~ InOut, data.raw)
