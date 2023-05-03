f36d22b6315c184c31b60fdb56114f5b468320da
Péter Kalicz <kaliczp@gmail.com>
2023-05-03 13:09:52 +0200

    Combined boxplots

--- Readme.R
+++ Readme.R
@@ -15,3 +15,26 @@ t.test(wMar ~ InOut, data.raw)
 
 ## Boxplot vagy doboz ábra
 boxplot(wOkt ~ InOut, data.raw, ylab = "WWC Október")
+
+
+## Nedvesség változás
+WWC <- data.frame(InOut = rep(data.raw$InOut, 4), Month = factor(rep(c("Okt", "Dec", "Feb", "Mar"), each = nrow(data.raw))), WWC = c(data.raw$wOkt, data.raw$wDec, data.raw$wFeb, data.raw$wMar))
+
+boxplot(WWC ~ InOut, WWC)
+
+## InOut tömbben
+WWC$InOMonth  <- factor(paste0(WWC$InOut, WWC$Month),
+                        levels = c("IOkt", "IDec", "IFeb", "IMar", "OOkt", "ODec", "OFeb", "OMar"))
+
+## InOut havonta
+WWC$InOMonth  <- factor(paste0(WWC$InOut, WWC$Month),
+                        levels = c("IOkt", "OOkt", "IDec", "ODec", "IFeb", "OFeb", "IMar", "OMar"))
+
+boxplot(WWC ~ InOMonth, WWC)
+
+## havontára
+boxplot(WWC ~ InOMonth, WWC, xlab = "", xaxt = "n")
+axis(1, at = 1:8, labels = rep(c("I", "O"), 4))
+par(mgp = c(3,2.5,0))
+axis(1, at = c(1.5, 3.5, 5.5, 7.5), labels = c("Október", "December", "Február", "Március"), tcl = 0)
+par(mgp = c(3,1,0))
