1f73a521e7742a7cb1ec0b47c2f97c1c56ef0599
Péter Kalicz <kaliczp@gmail.com>
2023-05-03 09:16:44 +0200

    Import dataset
    Import with readxl package.

--- Readme.R
+++ Readme.R
@@ -1,3 +1,6 @@
 ## Látszik-e az xlsx?
 dir()
-
+## Beolvasás
+library(readxl)
+data.raw <- as.data.frame(read_excel("F_L_2023_complete_soil_dataset_T_probara.xlsx"))
+names(data.raw) <- c("ID", "InOut", "wOkt", "wDec", "wFeb", "wMar", "meanh(cm)", "Dens(db/m2)")
